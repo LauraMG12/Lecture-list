6bf71efa17e76ab04f78b9448731737b8d35d694
Laura Mañogil <laura.manogil@sembo.se>
2023-10-07 15:35:31 +0200

    configuration

---
+++ src/state/store.ts
@@ -0,0 +1,12 @@
+import { defineStore } from "pinia";
+
+export const useStore = defineStore("lectureList", {
+  // DATA -> ref()
+  state: () => ({}),
+
+  // COMPUTED -> computed()
+  getters: {},
+
+  // METHODS -> function()
+  actions: {},
+});
